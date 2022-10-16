b222f8bdfac8c74e7ba8eb307a87c559ab481243
mahmoudgomaa99 <mahmoud.gomaa14@gmail.com>
2022-10-16 11:01:41 +0200

    appcenter edits

--- android/app/src/main/java/com/instabadel_mobileapp/MainApplication.java
+++ android/app/src/main/java/com/instabadel_mobileapp/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.soloader.SoLoader;
 import com.instabadel_mobileapp.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.microsoft.codepush.react.CodePush;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -22,6 +23,11 @@ public class MainApplication extends Application implements ReactApplication {
           return BuildConfig.DEBUG;
         }
 
+         @Override
+        protected String getJSBundleFile() {
+            return CodePush.getJSBundleFile();
+        }
+        
         @Override
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
